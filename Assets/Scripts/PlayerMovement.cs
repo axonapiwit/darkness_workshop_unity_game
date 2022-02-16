bdab7d766a19592ac50ae94e710522bed6cae895
apiwit <axon.apiwit@gmail.com>
2022-02-17 02:41:49 +0700

    Player Flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{   
-    private Rigidbody2D body;
+{
     [SerializeField] private float speed;
-    
-    private void Awake()
+    private Rigidbody2D body;
+    private void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
-    {   
-        // float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Player Flip
+        if (horizontalInput > 0.05f)
+            transform.localScale = new Vector3(5, 5, 5);
+        else if (horizontalInput < -0.05f)
+            transform.localScale = new Vector3(-5, 5, 5);
 
         // Player jump
         if (Input.GetKey(KeyCode.W))
